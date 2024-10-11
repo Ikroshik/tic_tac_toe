949473167a95fc2a579910202f9774bc5b75580b
Ikroshik <khakimov7076@gmail.com>
2024-10-11 10:49:07 +0500

    added time travel

--- src/App.js
+++ src/App.js
@@ -12,22 +12,13 @@ function Square({ value, onSquareClick }) {
   )
 }
 
-// мейн функция дял реакта(помечается как 'default'), тут это функция доски, для игры в "крестики нолики", в ней: 
+// функция доски для игры в "крестики нолики", в ней: 
 // создается поле, записывается в клетку крестик или нолик(в зависимости от хода), считывается победитель или ничья
-export default function Board() {
-  const [xIsNext, setIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const winner = calculateWinner(squares);
-  let status;
+function Board({ xIsNext, squares, onPlay }) {
 
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O")
-  }
-
+  // функция рендеринга крестика или нолика, и определения победителя
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
 
@@ -39,8 +30,16 @@ export default function Board() {
       nextSquares[i] = "O";
     }
 
-    setSquares(nextSquares);
-    setIsNext(  xIsNext)
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O")
   }
 
   return (
@@ -65,6 +64,52 @@ export default function Board() {
   );
 }
 
+// мейн функция для реакта(помечается как 'default'), котоая говорит реакту использовать её как "top-level component",
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  // функция, которая вызывается Boardом для ре-рендеринга игры
+  function handleplay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    console.log(nextHistory)
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    
+    return (
+      <li key={ move }>
+        <button onClick={() => jumpTo(move)}>{ description }</button>
+      </li>
+    )
+  })
+
+  return (
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handleplay} />
+        </div>
+        <div className="game-info">
+          <ol>{ moves }</ol>
+        </div>
+      </div>
+  )
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -79,11 +124,11 @@ function calculateWinner(squares) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    console.log(squares[a])
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       // console.log(squares[a])
       return squares[a]
     }
     
   }
+  return null
 }
